fe4d01c05f55f619b86e01baecd91d576e4dad09
Fla64 <flavio_04_12@hotmail.com>
2017-12-03 23:56:04 -0600

    Add una clase para las consultas sql

---
+++ src/agencias_de_viajes/Consultas_sql_1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agencias_de_viajes;
+
+/**
+ *
+ * @author Jose Lopez
+ */
+public class Consultas_sql_1 {
+    
+}
