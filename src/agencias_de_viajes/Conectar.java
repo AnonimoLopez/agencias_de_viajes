3a40153e7224e563baffa056d36c7e2b9806105b
AnonimoLopez <jose_lopez_emo@hotmail.com>
2017-12-03 23:26:51 -0600

    Inicia

---
+++ src/agencias_de_viajes/Conectar.java
@@ -0,0 +1,47 @@
+package agencias_de_viajes;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author flavioantonio
+ */ 
+public class Conectar {
+   // private static String dba = "unalm";
+private static String user = "root";
+    private static String pass = "";
+    private static String url = "jdbc:mysql://localhost:3306/vuelos";
+    private static Connection conn;
+    private static Connection x;
+   
+    public static Connection geConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(url, user, pass);
+            //conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/villalida","root","");
+            System.out.println("exito");
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null," Error En La Base De Datos " + e.getMessage());
+
+        }
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+           geConnection();
+           
+        
+    }
+}
